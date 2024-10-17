3479aad6f34e6d99b13c7de3da346e0e7c2c69e5
ZitongWang1202 <zitongw961202@gmail.com>
2024-10-17 16:26:26 -0400

    start rules layout part

--- pages/game_selection_rules/game_selection_rules.vue
+++ pages/game_selection_rules/game_selection_rules.vue
@@ -1,6 +1,20 @@
 <template>
   <view>
-    rules
+    <view class="scroll-view-container">
+      <!-- 左侧的滚动视图区域，之后改成固定也可以 -->
+      <scroll-view class="left-scroll-view" scroll-y="true" >
+        <view class="number-of-player">三人游戏</view>
+        <view class="number-of-player">四人游戏</view>
+        <view class="number-of-player">五人游戏</view>
+        <view class="number-of-player">六人游戏</view>
+      </scroll-view>
+      <!-- 右侧flex布局，下面的代码目前只用于占位 -->
+      <view class="right-img-box">
+          <view class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0">
+            <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+          </view>
+        </view>
+    </view>
   </view>
 </template>
 
