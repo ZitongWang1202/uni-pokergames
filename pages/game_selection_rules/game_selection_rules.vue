6471c9e2e8c73c825ba795d816d04893981f62dc
ZitongWang1202 <zitongw961202@gmail.com>
2024-10-28 17:15:17 -0400

    add html css to rules page

--- pages/game_selection_rules/game_selection_rules.vue
+++ pages/game_selection_rules/game_selection_rules.vue
@@ -1,19 +1,25 @@
 <template>
   <view>
-    <view class="scroll-view-container">
-      <!-- 左侧的滚动视图区域，之后改成固定也可以 -->
-      <scroll-view class="left-scroll-view" scroll-y="true" >
+    <view class="container">
+      <!-- 左边部分，Flex 布局 -->
+      <view class="left">
         <view class="number-of-player">三人游戏</view>
         <view class="number-of-player">四人游戏</view>
         <view class="number-of-player">五人游戏</view>
         <view class="number-of-player">六人游戏</view>
-      </scroll-view>
-      <!-- 右侧flex布局，下面的代码目前只用于占位 -->
-      <view class="right-img-box">
-          <view class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0">
-            <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
-          </view>
-        </view>
+      </view>
+    
+      <!-- 右边部分，Grid 布局 -->
+      <view class="right">
+        <view class="grid-item">Long Rectangle 1</view>
+        <view class="grid-item">Square 2</view>
+        <view class="grid-item">Square 3</view>
+        <view class="grid-item">Square 4</view>
+        <view class="grid-item">Square 5</view>
+        <view class="grid-item">Square 6</view>
+        <view class="grid-item">Square 7</view>
+        <view class="grid-item">Square 8</view>
+      </view>
     </view>
   </view>
 </template>
@@ -22,12 +28,54 @@
   export default {
     data() {
       return {
-        
+
       };
     }
   }
 </script>
 
 <style lang="scss">
+  .container {
+    display: flex;
+    width: 100%;
+    // height: 100vh;
+    flex-direction: row;
+  }
 
+  .left {
+    width: 100px;
+    display: flex; 
+    flex-direction: column;
+    background-color: #f5f5f5;
+    border-right: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+
+  .number-of-player {
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+    font-size: 14px;
+    border-bottom: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  
+.right {
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);    // 3列等宽
+}
+
+.grid-item {
+  background-color: #fff;
+  border: 1px solid #ddd;
+  min-height: 100px;  // 设置最小高度确保格子形状
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.grid-item:first-child {
+  grid-column: 1 / 3;  // 从第1列开始，到第3列结束（占2格）
+}
 </style>
